bc539e31784cba2618928ab53045f59c5ab4d87f
Mathias Wellner <mathias@mwellner.de>
2016-04-19 16:38:21 +0200

    refactored index route for result app

--- result/routes/index.js
+++ result/routes/index.js
@@ -5,20 +5,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  getCountsFromDb(res);
+});
+
+function getCountsFromDb(res) {
   db.any("SELECT vote, COUNT(*) AS count FROM votes GROUP BY vote ORDER BY vote")
 	  .then(function(data) {
 		  var count1 = parseInt(data[0].count);
 		  var count2 = parseInt(data[1].count);
 		  var count3 = parseInt(data[2].count);
-  		res.render('index', { 
-			  count1: count1, 
-				count2: count2, 
-				count3: count3,
-				count: count1 + count2 + count3});
+  		var counts = { count1: count1, count2: count2, count3: count3, count: count1 + count2 + count3 }
+      res.render('index', counts);
 		})
 		.catch(function(error) {
-		  res.render('index', { count1: 0, count2: 0, count3: 0, count: 0 });
+      console.log("error getting counts!");
+      res.render('error', {message: "Could not get counts", error: error});
 		});
-});
+}
 
 module.exports = router;
