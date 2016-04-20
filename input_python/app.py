d1ca9bc91c7e2a5f6342d666b463b1b4d0f4b08b
Mathias Wellner <mathias@mwellner.de>
2016-04-20 14:19:32 +0200

    adapted test plan, improved styling

--- input_python/app.py
+++ input_python/app.py
@@ -15,5 +15,9 @@ def input():
     redisClient.rpush('votes', request.form['vote'])    
     return render_template('confirm.html')
 
+@app.route("/about")
+def about():
+  return render_template('about.html')
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
--- result/routes/index.js
+++ result/routes/index.js
@@ -4,23 +4,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  getCountsFromDb(res);
-});
-
-function getCountsFromDb(res) {
+router.get('/result', function(req, res, next) {
   db.any("SELECT vote, COUNT(*) AS count FROM votes GROUP BY vote ORDER BY vote")
 	  .then(function(data) {
 		  var count1 = parseInt(data[0].count);
 		  var count2 = parseInt(data[1].count);
 		  var count3 = parseInt(data[2].count);
-  		var counts = { count1: count1, count2: count2, count3: count3, count: count1 + count2 + count3 }
+  		var counts = { count1: count1, count2: count2, count3: count3, count: count1 + count2 + count3, title: "Voting Results" }
       res.render('index', counts);
 		})
 		.catch(function(error) {
       console.log("error getting counts!");
       res.render('error', {message: "Could not get counts", error: error});
 		});
-}
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
 
 module.exports = router;
