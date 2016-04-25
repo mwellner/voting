58f8896c647b0a4b9e9e26212611d4a66a6f5ada
Mathias Wellner <mathias@mwellner.de>
2016-04-25 21:09:20 +0200

    used predefined java worker

--- input-python/input.py
+++ input-python/input.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-
+import random
 import redis
+import json
+
 redisClient = redis.StrictRedis(host='redis', port=6379, db=0)
 
 app = Flask(__name__)
@@ -12,7 +14,9 @@ def input():
   if request.method == 'GET':
     return render_template('input.html')
   else:
-    redisClient.rpush('votes', request.form['vote'])    
+    voter_id = hex(random.getrandbits(64))[2:-1]
+    data = json.dumps({'voter_id': voter_id, 'vote': request.form['vote']})
+    redisClient.rpush('votes', data) 
     return render_template('confirm.html')
 
 @app.route("/about")
